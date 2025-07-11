16500ff570e6d03830f8918783f613595cf5b00c
Shady Elia <shady.elia@hotmail.com>
2025-07-11 15:56:29 +0300

    update logout code

--- library.js
+++ library.js
@@ -75,7 +75,7 @@ plugin.init = async function ({ router, middleware }) {
     }
   );
 
-  router.post("/auth/saml/logout", async (req, res) => {
+  router.get("/auth/saml/logout", async (req, res) => {
     winston.info("[sso-saml] auth/saml/logout triggered");
 
     const uid = req.user?.uid;
